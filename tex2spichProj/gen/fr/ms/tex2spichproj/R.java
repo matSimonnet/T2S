9e5cb38e582c4b5cf329d4d96df7004afa2ec8a7
Mathieu Simonnet Android <mat.simonnet@gmail.com>
2012-10-16 17:34:52 +0200

    new checkbox

--- tex2spichProj/gen/fr/ms/tex2spichproj/R.java
+++ tex2spichProj/gen/fr/ms/tex2spichproj/R.java
@@ -18,18 +18,22 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
+        public static final int autoCheckBox=0x7f080007;
         public static final int bearing=0x7f080002;
-        public static final int buttonBearing=0x7f08000a;
-        public static final int buttonSpeak=0x7f08000b;
-        public static final int buttonSpeed=0x7f080009;
+        public static final int bearingAutoCheckBox=0x7f080006;
+        public static final int buttonBearing=0x7f08000e;
+        public static final int buttonSpeak=0x7f08000f;
+        public static final int buttonSpeed=0x7f08000d;
         public static final int linearLayout1=0x7f080000;
-        public static final int linearLayout1bis=0x7f080004;
-        public static final int linearLayout2=0x7f080008;
-        public static final int menu_settings=0x7f08000c;
-        public static final int seekBarBearing=0x7f080006;
-        public static final int seekBarSpeed=0x7f080005;
-        public static final int seekBarTime=0x7f080007;
+        public static final int linearLayout2=0x7f080004;
+        public static final int linearLayout3=0x7f080008;
+        public static final int linearLayout4=0x7f08000c;
+        public static final int menu_settings=0x7f080010;
+        public static final int seekBarBearing=0x7f08000a;
+        public static final int seekBarSpeed=0x7f080009;
+        public static final int seekBarTime=0x7f08000b;
         public static final int speak=0x7f080003;
+        public static final int speedAutoCheckBox=0x7f080005;
         public static final int speedView=0x7f080001;
     }
     public static final class layout {
